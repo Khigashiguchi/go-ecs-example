a2cba61c6c03b6eb22ae449346c51a65d22f5617
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-08-24 14:41:35 +0900

    use ssm & kms

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+PARAMETER_STORE_PREFIX=${PARAMETER_STORE_PREFIX:-}
+if [ -n "$PARAMETER_STORE_PREFIX" ]; then
+    export DB_USER=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}.database.user --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_PASSWORD=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}.database.password --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_HOST=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}.database.host --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_NAME=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}.database.name --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_PORT=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}.database.port --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+fi
+
+exec "$@"
