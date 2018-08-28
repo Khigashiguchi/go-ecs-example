036cac9f7003d00eb1558a7f5a229b1c04cdd52d
Kazuki Higashiguchi <econoics0it0bem0116@gmail.com>
2018-08-28 21:18:10 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 
 PARAMETER_STORE_PREFIX=${PARAMETER_STORE_PREFIX:-}
 if [ -n "$PARAMETER_STORE_PREFIX" ]; then
-    export DB_USER=$(aws ssm get-parameters --name /goecssample/database/sample/master/user --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export DB_PASSWORD=$(aws ssm get-parameters --name /goecssample/database/sample/master/password --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export DB_HOST=$(aws ssm get-parameters --name /goecssample/database/sample/master/host --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export DB_NAME=$(aws ssm get-parameters --name /goecssample/database/sample/master/name --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export DB_PORT=$(aws ssm get-parameters --name /goecssample/database/sample/master/port --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_USER=$(aws ssm get-parameters --name /${PARAMETER_STORE_PREFIX}/database/sample/master/user --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_PASSWORD=$(aws ssm get-parameters --name /${PARAMETER_STORE_PREFIX}/database/sample/master/password --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_HOST=$(aws ssm get-parameters --name /${PARAMETER_STORE_PREFIX}/database/sample/master/host --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_NAME=$(aws ssm get-parameters --name /${PARAMETER_STORE_PREFIX}/database/sample/master/name --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+    export DB_PORT=$(aws ssm get-parameters --name /${PARAMETER_STORE_PREFIX}/database/sample/master/port --query "Parameters[0].Value" --region ap-northeast-1 --output text)
 fi
 
 exec "$@"
